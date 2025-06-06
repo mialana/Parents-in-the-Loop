bf0c5cd293523f5f717928e5e0723a702ef9669e
mialana <96770662+mialana@users.noreply.github.com>
2025-06-06 15:03:11 -0400

    Implement basic question answer mcp-agent

---
+++ backend/hack4impact/api/agent.py
@@ -0,0 +1,65 @@
+import asyncio
+import time
+
+from pydantic import BaseModel
+
+from mcp_agent.agents.agent import Agent
+from mcp_agent.app import MCPApp
+from mcp_agent.workflows.llm.augmented_llm_google import GoogleAugmentedLLM
+
+app = MCPApp(
+    name="mcp_basic_agent"
+)
+
+
+class Essay(BaseModel):
+    title: str
+    body: str
+    conclusion: str
+
+
+async def example_usage():
+    async with app.run() as agent_app:
+        logger = agent_app.logger
+        context = agent_app.context
+
+        logger.info("Current config:", data=context.config.model_dump())
+
+        finder_agent = Agent(
+            name="finder",
+            instruction="""You are an agent with the ability to fetch URLs. Your job is to identify
+            the closest match to a user's request, make the appropriate tool calls,
+            and return the URI and CONTENTS of the closest match.""",
+            server_names=["filesystem"],
+        )
+
+        async with finder_agent:
+            logger.info("finder: Connected to server, calling list_tools...")
+            result = await finder_agent.list_tools()
+            logger.info("Tools available:", data=result.model_dump())
+
+            llm = await finder_agent.attach_llm(GoogleAugmentedLLM)
+
+            result = await llm.generate_str(
+                message="Print the first 2 paragraphs of https://modelcontextprotocol.io/introduction",
+            )
+            logger.info(f"First 2 paragraphs of Model Context Protocol docs: {result}")
+
+            result = await llm.generate_structured(
+                message="Create a short essay using the first 2 paragraphs.",
+                response_model=Essay,
+            )
+            logger.info(f"Structured paragraphs: {result}")
+
+
+def main():
+    start = time.time()
+    asyncio.run(example_usage())
+    end = time.time()
+    t = end - start
+
+    print(f"Total run time: {t:.2f}s")
+
+
+if __name__ == "__main__":
+    main()
