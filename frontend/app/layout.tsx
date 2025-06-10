dd4283bef4c265bb0e2a66635686d0a9946ccf05
mialana <96770662+mialana@users.noreply.github.com>
2025-06-10 13:18:09 -0400

    Lint

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Parents in the Loop',
+  description: 'An AI-powered interface targeted towards school systems to bridge knowledge and language barriers for immigrant parents.',
 }
 
 export default function RootLayout({
